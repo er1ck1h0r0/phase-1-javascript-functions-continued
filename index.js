ad03710fc888cd3bacd69c7ac206844bd8397ba6
eric <ericmaina11455@gmail.com>
2024-03-20 21:48:16 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const saturdayFun = function(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+};
+
+const mondayWork = function(defaultStatement = "go to the office") {
+    return `This Monday, I will ${defaultStatement}.`;
+};
+
+
+const wrapAdjective = function(wrapper) {
+    return function(adjective) {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+};
